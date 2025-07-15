cfdaca270876939e43da856feb2f10beb41bacc5
Willianlq <68751621+willian-coder@users.noreply.github.com>
2025-07-15 01:16:41 +0000

    Batalha Naval - Aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+#define TAMANHO_NAVIO 3
+#define CAMPO 3
+
 int main()
 {
     int tabuleiro[10][10];
@@ -19,24 +22,48 @@ int main()
 
     // Posicao dos Navios
 
-    //vermelho
+    // vermelho
     int linhaVermelho = 1;
     int colunaVermelho = 2;
 
-    for (int i = 0; i < 3; i++)
+    for (int i = 0; i < TAMANHO_NAVIO; i++)
     {
-        tabuleiro[linhaVermelho][colunaVermelho + i] = 3;
-        navioVermelho[i] = colunaVermelho + i;
+        tabuleiro[linhaVermelho][colunaVermelho + i] = CAMPO;
+
     }
 
-    //azul
+    // azul
     int linhaAzul = 5;
     int colunaAzul = 5;
 
-    for (int i = 0; i < 3; i++)
+    for (int i = 0; i < TAMANHO_NAVIO; i++)
     {
-        tabuleiro[linhaAzul + i][colunaAzul] = 3;
-        navioAzul[i] = linhaAzul + i;
+           tabuleiro[linhaAzul + i][colunaAzul] = CAMPO;
+
+    }
+
+    int linhaDiagonalVermelha = 0;
+    int colunaDiagonalVermelha = 0;
+
+    // Navio Vermelho Diagonal
+    for (int i = 0; i < TAMANHO_NAVIO; i++)
+    {
+        if (tabuleiro[linhaDiagonalVermelha + i][colunaDiagonalVermelha + i] == 0)
+        {
+            tabuleiro[linhaDiagonalVermelha + i][colunaDiagonalVermelha + i] = CAMPO;
+        }
+    }
+
+    int linhaDiagonalAzul = 2;
+    int colunaDiagonalAzul = 9;
+
+    // Navio Azul diagonal
+    for (int i = 0; i < TAMANHO_NAVIO; i++)
+    {
+        if (tabuleiro[linhaDiagonalAzul + i][colunaDiagonalAzul - i] == 0)
+        {
+            tabuleiro[linhaDiagonalAzul + i][colunaDiagonalAzul - i] = CAMPO;
+        }
     }
 
     // Exibição do tabuleiro
@@ -50,13 +77,13 @@ int main()
     }
     printf("\n");
 
-    //Corpo
+    // Corpo
     for (int i = 0; i < 10; i++)
     {
         printf("%2d |", i + 1); // Imprime número da linha
         for (int j = 0; j < 10; j++)
         {
-            printf(" %d ", tabuleiro[i][j]); // Imprime valor (0 = água, 3 = navio)
+            printf(" %d ", tabuleiro[i][j]); // Imprime valor 0 = água, 3 = navio)
         }
         printf("\n");
     }
